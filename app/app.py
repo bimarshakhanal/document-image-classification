455da9f05a892c97eb3971a916d9683aa8eef873
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-05-15 10:29:38 +0545

    feat: Add confidence score in streamlit app

--- app/app.py
+++ app/app.py
@@ -5,10 +5,11 @@ Streamlit application to predict document class
 import streamlit as st
 from PIL import Image
 import torch
+from torch.nn import Softmax
 from torchvision.transforms import v2
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-doc_classes = ['Citizenship', 'License', 'Passport']
+doc_classes = ['Citizenship', 'License', 'Other', 'Passport']
 transform = v2.Compose([
     v2.ToPILImage(),
     v2.Resize((224, 224)),
@@ -24,20 +25,28 @@ st.header('Document Image Classification')
 st.info('Classify document image into citizenship, passport,license and other.')
 
 with st.spinner(text='Loading classification model'):
-    model = torch.load('resnet18.pth', map_location=torch.device('cpu'))
+    model = torch.load('resnet_all.pth', map_location=torch.device('cpu'))
     model = model.to(device)
 
 
 def predict(img):
     '''Predict document class'''
+    # Some png image can have an extra alpha channel
+    # so extract first three channels
     img = v2.ToTensor()(img)[:3]
+    # apply image transformation
     img = transform(img)
+    # batch the image input
     img = torch.unsqueeze(img, 0)
     img = img.to(device)
-    print("IM shape:", img.shape)
-    probs = model(img)
-    doc = torch.argmax(probs)
-    return doc.item()
+    # get logit outputs and use softmax to get probabilities
+    logits = model(img)
+    probs = Softmax(dim=1)(logits).squeeze(0)
+
+    # get document class index
+    doc = torch.argmax(probs).item()
+
+    return doc_classes[doc], probs[doc].item()
 
 
 file_up = st.file_uploader(
@@ -53,8 +62,7 @@ with col1:
 with col2:
     try:
         if IMAGE:
-            doc_class = predict(IMAGE)
-            st.success(f"The document is classified as {
-                       doc_classes[doc_class]}")
+            doc_class, conf = predict(IMAGE)
+            st.success(f"Document Class: {doc_class}: {round(conf*100, 2)}%")
     except SystemError:
         st.error("Failed to make prediction")
